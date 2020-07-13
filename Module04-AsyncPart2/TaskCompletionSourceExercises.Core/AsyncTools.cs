66a0ab335af063f9717ec2913d9460f62f88086a
Michal Berner <michal.berner@km.kongsberg.com>
2020-07-13 19:44:14 +0200

    async process implementation

--- Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
+++ Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
@@ -4,11 +4,42 @@ using System.Threading.Tasks;
 
 namespace TaskCompletionSourceExercises.Core
 {
-    public class AsyncTools
-    {
-        public static Task<string> RunProgramAsync(string path, string args = "")
-        {
-            return Task.FromResult(string.Empty);
-        }
-    }
+	public class AsyncTools
+	{
+		public static Task<string> RunProgramAsync(string path, string args = "")
+		{
+			var process = new Process();
+			process.EnableRaisingEvents = true;
+			process.StartInfo = new ProcessStartInfo(path, args)
+			{
+				RedirectStandardOutput = true,
+				RedirectStandardError = true
+			};
+
+			var tcs = new TaskCompletionSource<string>();
+			
+			process.Exited += async (sender, eventArgs) =>
+			{
+				var senderProcess = sender as Process;
+
+				var error = await senderProcess.StandardError.ReadToEndAsync().ConfigureAwait(false);
+
+				if (!string.IsNullOrEmpty(error))
+				{
+					tcs.SetException(new Exception(error));
+				}
+				else
+				{
+					var output = await senderProcess.StandardOutput.ReadToEndAsync().ConfigureAwait(false);
+					tcs.SetResult(output);
+				}
+
+				senderProcess?.Dispose();
+			};
+
+			process.Start();
+			
+			return tcs.Task;
+		}
+	}
 }
