f9dfb4a76064ebf043c3fc60573ca87174d545be
Michal Berner <michal.berner@km.kongsberg.com>
2020-07-13 19:32:04 +0200

    bool awaiter implementation

--- Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
+++ Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
@@ -3,15 +3,33 @@ using System.Runtime.CompilerServices;
 
 namespace AwaitableExercises.Core
 {
-    public static class BoolExtensions
-    {
-    }
+	public static class BoolExtensions
+	{
+		public static BoolAwaiter GetAwaiter(this bool value)
+		{
+			return new BoolAwaiter(value);
+		}
+	}
 
-    public class BoolAwaiter : INotifyCompletion
-    {
-        public void OnCompleted(Action continuation)
-        {
-            throw new NotImplementedException();
-        }
-    }
+	public class BoolAwaiter : INotifyCompletion
+	{
+		private bool _value;
+
+		public BoolAwaiter(bool value)
+		{
+			_value = value;
+		}
+
+		public bool IsCompleted { get; } = true;
+		
+		public void OnCompleted(Action continuation)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool GetResult()
+		{
+			return _value;
+		}
+	}
 }
