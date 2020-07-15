33526930b22baf28bdc644d2453f8caf8c1ad2c7
Michal Berner <michal.berner@km.kongsberg.com>
2020-07-15 21:52:10 +0200

    AsyncTools refactor

--- Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
+++ Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
@@ -8,32 +8,32 @@ namespace TaskCompletionSourceExercises.Core
 	{
 		public static Task<string> RunProgramAsync(string path, string args = "")
 		{
-			var process = new Process();
-			process.EnableRaisingEvents = true;
-			process.StartInfo = new ProcessStartInfo(path, args)
+			var process = new Process
 			{
-				RedirectStandardOutput = true,
-				RedirectStandardError = true
+				EnableRaisingEvents = true,
+				StartInfo = new ProcessStartInfo(path, args)
+				{
+					RedirectStandardOutput = true, RedirectStandardError = true
+				}
 			};
 
-			var tcs = new TaskCompletionSource<string>();
+			var tcs = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
 			
 			process.Exited += async (sender, eventArgs) =>
 			{
 				var senderProcess = sender as Process;
 
-				var error = await senderProcess.StandardError.ReadToEndAsync().ConfigureAwait(false);
-
-				if (!string.IsNullOrEmpty(error))
-				{
-					tcs.SetException(new Exception(error));
-				}
-				else
+				if (process.ExitCode == 0)
 				{
 					var output = await senderProcess.StandardOutput.ReadToEndAsync().ConfigureAwait(false);
 					tcs.SetResult(output);
 				}
-
+				else
+				{
+					var error = await senderProcess.StandardError.ReadToEndAsync().ConfigureAwait(false);
+					tcs.SetException(new Exception(error));
+				}
+				
 				senderProcess?.Dispose();
 			};
 
