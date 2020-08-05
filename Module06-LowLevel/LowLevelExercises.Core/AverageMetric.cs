b99f2fbafb9c10dcdaca087f3f0fbf2f8a669cc0
Michal Berner <michal.berner@km.kongsberg.com>
2020-08-05 22:18:35 +0200

    Module 06: AverageMetric implementation.

--- Module06-LowLevel/LowLevelExercises.Core/AverageMetric.cs
+++ Module06-LowLevel/LowLevelExercises.Core/AverageMetric.cs
@@ -1,52 +1,35 @@
-﻿using System.Threading;
+﻿using System.Numerics;
+using System.Threading;
 
 namespace LowLevelExercises.Core
 {
-    /// <summary>
-    /// A simple class for reporting a specific value and obtaining an average.
-    /// </summary>
-    /// TODO: remove the locking and use <see cref="Interlocked"/> and <see cref="Volatile"/> to implement a lock-free implementation.
-    public class AverageMetric
-    {
-        // TODO: this should not be needed, once you remove all the locks below
-        readonly object sync = new object();
+	/// <summary>
+	/// A simple class for reporting a specific value and obtaining an average.
+	/// </summary>
+	/// TODO: remove the locking and use <see cref="Interlocked"/> and <see cref="Volatile"/> to implement a lock-free implementation.
+	public class AverageMetric
+	{
+		private volatile int _sum = 0;
+		private volatile int _count = 0;
 
-        int sum = 0;
-        int count = 0;
+		public void Report(int value)
+		{
+			Interlocked.Add(ref _sum, value);
+			Interlocked.Increment(ref _count);
+		}
 
-        public void Report(int value)
-        {
-            // TODO: how to increment sum + count without locking?
-            lock (sync)
-            {
-                sum += value;
-                count += 1;
-            }
-        }
+		public double Average => Calculate(_count, _sum);
 
-        public double Average
-        {
-            get
-            {
-                // TODO: how to access the values in a lock-free way?
-                // let's assume that we can return value estimated on a bit stale data(in time average will be less and less diverged)
-                lock (sync)
-                {
-                    return Calculate(count, sum);
-                }
-            }
-        }
+		private static double Calculate(in int count, in int sum)
+		{
+			// DO NOT change the way calculation is done.
 
-        static double Calculate(in int count, in int sum)
-        {
-            // DO NOT change the way calculation is done.
+			if (count == 0)
+			{
+				return double.NaN;
+			}
 
-            if (count == 0)
-            {
-                return double.NaN;
-            }
-
-            return (double)sum / count;
-        }
-    }
+			return (double)sum / count;
+		}
+	}
 }
