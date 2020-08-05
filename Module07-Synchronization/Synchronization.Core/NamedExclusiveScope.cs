d5daeb67c852569ec461c6518945f7e3c8519535
Michal Berner <michal.berner@km.kongsberg.com>
2020-08-05 23:22:47 +0200

    Module 07: NamedExclusiveScope implementation

--- Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
+++ Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
@@ -3,16 +3,33 @@ using System.Threading;
 
 namespace Synchronization.Core
 {
-    /*
-     * Implement very simple wrapper around Mutex or Semaphore (remember both implement WaitHandle) to
-     * provide a exclusive region created by `using` clause.
-     *
-     * Created region may be system-wide or not, depending on the constructor parameter.
-     */
-    public class NamedExclusiveScope : IDisposable
-    {
-        public NamedExclusiveScope(string name, bool isSystemWide)
-        {
-        }
-    }
+	/*
+	 * Implement very simple wrapper around Mutex or Semaphore (remember both implement WaitHandle) to
+	 * provide a exclusive region created by `using` clause.
+	 *
+	 * Created region may be system-wide or not, depending on the constructor parameter.
+	 */
+	public class NamedExclusiveScope : IDisposable
+	{ 
+		private readonly Mutex _mutex;
+
+		public NamedExclusiveScope(string name, bool isSystemWide)
+		{
+			_mutex = new Mutex(true, isSystemWide ? $"Global\\{name}" : name, out var isCreatedNew);
+			
+			if (!isCreatedNew)
+			{
+				_mutex = null;
+				throw new InvalidOperationException($"Unable to get a global lock {name}.");
+			}
+
+			_mutex.WaitOne();
+		}
+
+		public void Dispose()
+		{
+			_mutex?.ReleaseMutex();
+			_mutex?.Dispose();
+		}
+	}
 }
